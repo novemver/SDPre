61e8434520a8d85b17d2d08af19948ca6528ae5a
Dom <dominicjackss@gmail.com>
2022-12-27 15:27:14 +0100

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+	public static void main (String[] args) {
+		String name = "Dominic Surina";
+		int age = 37;
+		String state = "California";
+
+		System.out.println(name + " Age: " + age + " State: " + state);
+	}
+}
