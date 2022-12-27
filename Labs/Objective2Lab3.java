0c8d7b0ebed9941092af8437fc50af0672399cfa
Dom <dominicjackss@gmail.com>
2022-12-27 15:40:49 +0100

    Complete objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+
+
+		int yearBorn = 1985;
+		String monthBorn = "September";
+		boolean isTodayTheWeekend = false;
+
+		System.out.println(yearBorn + "\n" + monthBorn + "\n" + isTodayTheWeekend);
+	}
+}
