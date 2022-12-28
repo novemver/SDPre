9b8d7d2c9832cd33791aaac6ab9035615b41b406
Dom <dominicjackss@gmail.com>
2022-12-28 17:32:40 +0100

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		
+		int sumTotal = 0;
+		
+		for (int i = 1; i < 21; i++) {
+			sumTotal += i;
+		}
+		System.out.println(sumTotal);
+	}
+}
