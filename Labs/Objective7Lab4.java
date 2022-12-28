109c0c37d1b267fbbe3eb26061449e1512030b22
Dom <dominicjackss@gmail.com>
2022-12-28 12:09:28 +0100

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int num1, num2;
+		int count = 0;
+		int sum = 0;
+
+		while(count <= 20) {
+			sum += count;
+			count++;
+		}
+		System.out.println(sum);
+
+	}
+
+}
